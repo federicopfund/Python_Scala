bbe329fb7dc74f1e290aaf4bec2f9b396985cbf8
federico pfund <federicopfund@gmail.com>
2024-01-24 15:06:52 -0300

    Inclusive

--- Ejercicios/ejercicios_python/Clase01/inclusive.py
+++ Ejercicios/ejercicios_python/Clase01/inclusive.py
@@ -1,20 +1,31 @@
 
+#%%
+class TextManipulator:
+    def __init__(self, text):
+        self.text = text
+        self.words = text.split()
 
-def neutralizar_palabras(palabra):
-    if len(palabra)>=2 and palabra[-2]=='0':
-        palabra_nueva=palabra[:-2] +'e'+ palabra[-1]
-    elif palabra[-1]=='o':
-        palabra_nueva[:-1] + 'e'
-    else:
-        palabra_nueva=palabra
-        
-    return palabra_nueva
+    def neutralize_word(self, word):
+        if len(word) >= 2 and word[-2] == '0':
+            return word[:-2] + 'e' + word[-1]
+        elif word[-1] == 'o':
+            return word[:-1] + 'e'
+        else:
+            return word
 
+    def neutralize_all_words_generator(self):
+        for word in self.words:
+            yield self.neutralize_word(word)
+
+    def manipulate_text(self):
+        neutralized_words = self.neutralize_all_words_generator()
+        return ' '.join(neutralized_words)
+
+#%%
+# Ejemplo de uso de la clase TextManipulator
 frase = '¿como trasmitir a los otros el infinito Aleph?'
-palabras = frase.split()
-palabras_nuevas= []
-for palabra in palabra:
-    palabra_nueva = neutralizar_palabras(palabra)
-    palabra_nueva.append(palabra_nueva)
-frase_t =' '.join(palabra_nueva)
+text_manipulator = TextManipulator(frase)
+frase_t = text_manipulator.manipulate_text()
+
 print(frase_t)
+
