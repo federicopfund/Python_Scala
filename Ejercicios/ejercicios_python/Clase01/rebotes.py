18cda226fa8f28af9eccc4db36975a6c5e270aa7
federico pfund <federicopfund@gmail.com>
2024-01-24 15:07:11 -0300

    Clase rebota pelota

--- Ejercicios/ejercicios_python/Clase01/rebotes.py
+++ Ejercicios/ejercicios_python/Clase01/rebotes.py
@@ -1,9 +1,21 @@
 #La pelota que rebota.
-altura = 100
-rebote = 0
-while rebote <= 11:
-    rebote += 1  
-    altura = altura *(3/5) 
-    print(rebote,round(altura,2))
+class BouncingBall:
+    def __init__(self, initial_height, bounces):
+        self.height = initial_height
+        self.bounces = bounces
+        self.current_bounce = 0
+
+    def simulate_bounces(self):
+        while self.current_bounce < self.bounces:
+            self.current_bounce += 1
+            self.height *= (3/5)
+            print(f"Bounce {self.current_bounce}: Height {round(self.height, 2)}")
+
+# Example of using the BouncingBall class
+initial_height = 100
+bounces = 12
+
+bouncing_ball = BouncingBall(initial_height, bounces)
+bouncing_ball.simulate_bounces()
 
 
