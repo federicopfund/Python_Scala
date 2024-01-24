cd70a01fabf20934517ca62e2b4226625ef0f4ec
federico pfund <federicopfund@gmail.com>
2024-01-24 15:00:40 -0300

    Clase geringozo.

--- Ejercicios/ejercicios_python/Clase01/geringoso.py
+++ Ejercicios/ejercicios_python/Clase01/geringoso.py
@@ -1,22 +1,32 @@
-dic = dict()
 
-def geringoso(lista):
-  
-    try:  
-        
-        for gerin in lista:
-            palabra_convertida = " "
-            palabra_convertida += " "
-            for goso in gerin:
-               if goso in "aeiou":
-                   palabra_convertida += goso + "p" + goso
-               else:
-                   palabra_convertida += goso
-            dic [gerin] = palabra_convertida
-        return (dic)
-        
-    except:
-        print("no admite alfanumericos")
-        
-    
-geringoso(['pera','mandarna','naranja'])
+
+class GeringosoConverter:
+    def __init__(self):
+        self.dic = {}
+
+    def convert_to_geringoso(self, word):
+        converted_word = ""
+        for char in word:
+            if char in "aeiou":
+                converted_word += char + "p" + char
+            else:
+                converted_word += char
+        return converted_word.strip()
+
+    def process_list(self, word_list):
+        try:
+            for gerin in word_list:
+                converted_word = self.convert_to_geringoso(gerin)
+                self.dic[gerin] = converted_word
+            return self.dic
+        except:
+            print("No admite alfanumericos")
+
+# Create an instance of the GeringosoConverter
+geringoso_instance = GeringosoConverter()
+
+# Use the instance to process a list of words
+result = geringoso_instance.process_list(['pera', 'mandarina', 'naranja'])
+
+# Display the result
+print(result)
