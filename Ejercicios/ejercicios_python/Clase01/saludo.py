b29713e55078f2c182d8f2f717c7c6a2c47c9948
federico pfund <federicopfund@gmail.com>
2024-01-24 15:07:22 -0300

    Saludo de dog

--- Ejercicios/ejercicios_python/Clase01/saludo.py
+++ Ejercicios/ejercicios_python/Clase01/saludo.py
@@ -1,2 +1,7 @@
-nombre = input("ingrese su nombre: ")
-print("Hola",nombre)
+class Dog:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def bark(self):
+        print("Woof!")
