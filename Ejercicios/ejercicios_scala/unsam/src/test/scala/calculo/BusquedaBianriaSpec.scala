343ccd2a25c3b65be271f337bf1f16851ce34c28
federico pfund <federicopfund@gmail.com>
2024-01-25 13:43:31 -0300

    Test de busqueda binaria

---
+++ Ejercicios/ejercicios_scala/unsam/src/test/scala/calculo/BusquedaBianriaSpec.scala
@@ -0,0 +1,26 @@
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.Assertions._
+import calculo.BusquedaBinaria
+
+class BusquedaBinariaTest extends AnyFunSuite {
+
+  test("busquedaBinaria should find the correct position of an element in the sorted list") {
+    val lista = List(5, 8, 12, 15, 22, 30, 34, 40)
+
+    assert(BusquedaBinaria.busquedaBinaria(lista, 22) === 4)
+  }
+
+  test("busquedaBinaria should return -1 for an element not present in the list") {
+    val lista = List(5, 8, 12, 15, 22, 30, 34, 40)
+
+    assert(BusquedaBinaria.busquedaBinaria(lista, 25) === -1)
+  }
+
+  test("busquedaBinaria should handle an empty list") {
+    val lista = List.empty[Int]
+
+    assert(BusquedaBinaria.busquedaBinaria(lista, 10) === -1)
+  }
+}
+
