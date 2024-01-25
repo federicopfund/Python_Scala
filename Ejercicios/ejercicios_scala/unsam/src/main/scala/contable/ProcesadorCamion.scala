63c325e610b2768bc85b57baf7c1824a2df1f7bb
federico pfund <federicopfund@gmail.com>
2024-01-24 22:16:44 -0300

    Porcesa camion , lee calcula total,diferencia y reaudacion.

---
+++ Ejercicios/ejercicios_scala/unsam/src/main/scala/contable/ProcesadorCamion.scala
@@ -0,0 +1,76 @@
+package contable
+
+import scala.io.Source
+
+object ProcesadorCamion {
+
+  def leerCamion(nuevoArchivo: String): List[Map[String, Any]] = {
+    try {
+      val lines = Source.fromFile(nuevoArchivo).getLines().toList
+      val headers = lines.head.split(",")
+      val camion = lines.tail.map { line =>
+        val fields = line.split(",")
+        Map(headers.zip(fields): _*)
+      }
+      camion
+    } catch {
+      case _: Throwable =>
+        println("No corresponde a una dirección válida.")
+        List.empty
+    }
+  }
+
+  def totalPagado(precioCompra: List[Map[String, Any]]): Double = {
+    precioCompra.map(s => s("cajones").asInstanceOf[Int] * s("precio").asInstanceOf[Double]).sum
+  }
+
+  def leerPrecios(nuevoArchivo: String): Map[String, Double] = {
+    try {
+      val lines = Source.fromFile(nuevoArchivo).getLines().toList
+      lines.flatMap { line =>
+        val fields = line.split(",")
+        if (fields.length == 2) Some(fields(0) -> fields(1).toDouble) else None
+      }.toMap
+    } catch {
+      case _: Throwable => Map.empty
+    }
+  }
+
+  def diferencia(precioCompra: List[Map[String, Any]], precioVenta: Map[String, Double]): Double = {
+    precioCompra.flatMap { s =>
+      precioVenta.get(s("nombre").asInstanceOf[String]).map { precio =>
+        val venta = precio * s("cajones").asInstanceOf[Int]
+        val compra = s("cajones").asInstanceOf[Int] * s("precio").asInstanceOf[Double]
+        venta - compra
+      }
+    }.sum
+  }
+
+  def recauda(precioCompra: List[Map[String, Any]], precioVenta: Map[String, Double]): Double = {
+    precioCompra.flatMap { s =>
+      precioVenta.get(s("nombre").asInstanceOf[String]).map { precio =>
+        precio * s("cajones").asInstanceOf[Int]
+      }
+    }.sum
+  }
+
+  def line(): Unit = {
+    println("-" * (14 * 7))
+  }
+
+  def mostrarPantalla(): Unit = {
+    val precioCompra = leerCamion("resources/camion.csv")
+    val totalPagadoEnCompra = totalPagado(precioCompra)
+    val precioVenta = leerPrecios("resource/precios.csv")
+    val diferenciaEnVentas = diferencia(precioCompra, precioVenta)
+    val recaudacionDeVentas = recauda(precioCompra, precioVenta)
+
+    line()
+    println("| nombre   | precio_venta  | diferencia_de_venta | recaudacion_de_venta | total_pagado_en_compra |")
+    line()
+    precioVenta.keys.foreach { e =>
+      println(f"|${e.padTo(9, ' ')} | ${precioVenta(e).toString.padTo(14, ' ')}| ${diferenciaEnVentas.toString.padTo(19, ' ')}  | ${recaudacionDeVentas.toString.padTo(22, ' ')} | ${totalPagadoEnCompra.toString.padTo(22, ' ')} |")
+    }
+    line()
+  }
+}
