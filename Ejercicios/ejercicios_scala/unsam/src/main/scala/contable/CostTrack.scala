560faf1ca1bb7f0ec6902e2a8e91f3c974c79689
federico pfund <federicopfund@gmail.com>
2024-01-26 16:34:37 -0300

    feat: Agregar función para calcular el costo total de la carga de un camión desde un archivo
    La función `costo` en el objeto `Camion` permite calcular el costo total de la carga de un camión a partir de un archivo de datos.
    
    Detalles del cambio:
    - La función utiliza Scala.io.Source para abrir el archivo y procesar los datos.
    - Excluye la primera línea del archivo (encabezados) y realiza el cálculo del costo total sumando el costo de cada línea.
    - Maneja posibles errores, como filas vacías o datos incorrectos, e imprime mensajes informativos.
    - Cierra el archivo al finalizar el procesamiento.
    
    Ejemplo de uso:
    
    ```scala
    val filePath = Paths.get("ruta/del/archivo/datos_camion.csv")
    val costoTotal = Camion.costo(filePath)
    println(s"El costo total de la carga del camión es: $$${costoTotal}")

--- Ejercicios/ejercicios_scala/unsam/src/main/scala/contable/CostTrack.scala
+++ Ejercicios/ejercicios_scala/unsam/src/main/scala/contable/CostTrack.scala
@@ -3,30 +3,47 @@ package contable
 import scala.io.Source
 import java.nio.file.Path
 
-
 object Camion {
+
+  /**
+    * Calcula el costo total de la carga de un camión a partir de un archivo de datos.
+    *
+    * @param nombreArchivo Ruta del archivo que contiene los datos de la carga del camión.
+    * @return Costo total de la carga del camión. Si hay filas vacías o errores en los datos, se imprime un mensaje y se devuelve 0.0.
+    */
   def costo(nombreArchivo: Path): Double = {
     try {
+      // Abre el archivo utilizando Scala.io.Source
       val a = Source.fromFile(nombreArchivo.toFile)
+      
+      // Obtiene las filas del archivo excluyendo la primera (encabezados)
       val rows = a.getLines().drop(1).map(_.split(","))
+      
       var precioFinal = 0.0
       var contador = 0
 
+      // Itera sobre cada línea del archivo
       for (line <- rows) {
         contador += 1
         try {
+          // Intenta obtener el número de cajones y el precio de la línea actual
           val cajon = line(1).toInt
           val precio = line(2).toDouble
+          // Calcula el costo total sumando el costo de cada línea
           precioFinal += cajon * precio
         } catch {
-          case _: Throwable => println(s"La fila $contador se encuentra vacía. ${line.mkString(", ")}")
+          case _: Throwable =>
+            // Imprime un mensaje si hay una fila vacía o un error en los datos
+            println(s"La fila $contador se encuentra vacía. ${line.mkString(", ")}")
         }
       }
 
+      // Cierra el archivo al finalizar
       a.close()
       precioFinal
     } catch {
       case _: Throwable =>
+        // Imprime un mensaje si hay un error al abrir el archivo
         println(s"${nombreArchivo.toString}\nNo corresponde a una dirección válida.")
         0.0
     }
