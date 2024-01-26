0307ec5741f708e39e551e2f3a6a22174052b26f
federico pfund <federicopfund@gmail.com>
2024-01-26 16:25:35 -0300

    feat: Agregar GeringosoConverter para convertir palabras al estilo "geringoso"
    La clase `GeringosoConverter` permite convertir palabras al estilo "geringoso", una jerga argentina donde se agrega 'p' entre cada vocal en una palabra.
    
    Detalles del cambio:
    - Se ha creado la clase `GeringosoConverter` con un diccionario interno para almacenar las palabras originales y sus versiones convertidas.
    - El método `convertToGeringoso` realiza la conversión de una palabra según las reglas del "geringoso".
    - El método `processList` procesa una lista de palabras y construye un diccionario con las versiones convertidas al "geringoso".
    - Se ha incluido manejo de errores para casos donde las palabras contienen caracteres alfanuméricos.
    
    Ejemplo de uso:
    
    ```scala
    val converter = new GeringosoConverter()
    val wordList = List("hola", "mundo", "geringoso")
    val resultDictionary = converter.processList(wordList)
    println(resultDictionary)

--- Ejercicios/ejercicios_scala/unsam/src/main/scala/varios/GeringosoConverter.scala
+++ Ejercicios/ejercicios_scala/unsam/src/main/scala/varios/GeringosoConverter.scala
@@ -1,10 +1,22 @@
 
 package varios
 
-
+/**
+  * Clase GeringosoConverter para convertir palabras al estilo "geringoso".
+  * El "geringoso" es una jerga argentina donde se agrega 'p' entre cada vocal en una palabra.
+  *
+  * @constructor Crea una instancia de GeringosoConverter con un diccionario vacío.
+  */
 class GeringosoConverter {
+  // Diccionario interno para almacenar las palabras originales y sus versiones convertidas.
   private var dic: Map[String, String] = Map()
 
+  /**
+    * Convierte una palabra al estilo "geringoso".
+    *
+    * @param word Palabra a convertir.
+    * @return Palabra convertida al estilo "geringoso".
+    */
   def convertToGeringoso(word: String): String = {
     var convertedWord = ""
     for (char <- word) {
@@ -17,6 +29,12 @@ class GeringosoConverter {
     convertedWord.trim
   }
 
+  /**
+    * Procesa una lista de palabras y construye un diccionario con las versiones convertidas al "geringoso".
+    *
+    * @param wordList Lista de palabras a procesar.
+    * @return Diccionario con palabras originales y sus versiones convertidas.
+    */
   def processList(wordList: List[String]): Map[String, String] = {
     try {
       for (gerin <- wordList) {
