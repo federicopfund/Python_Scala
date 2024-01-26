d8da518f7a3fdf5803adae75c8b429c292afc8c3
federico pfund <federicopfund@gmail.com>
2024-01-26 15:14:41 -0300

    Parser csv y construye rdd de csv park.

--- Ejercicios/ejercicios_scala/unsam/src/main/scala/Operation/CsvParserRecursive.scala
+++ Ejercicios/ejercicios_scala/unsam/src/main/scala/Operation/CsvParserRecursive.scala
@@ -1,25 +1,71 @@
 package Operation
 
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.types.{StructType, StructField, StringType, DoubleType,IntegerType}
 import org.apache.spark.sql.{SparkSession, DataFrame}
 import org.apache.spark.sql.functions.col
 import scala.io.Source
 import java.nio.file.{Path, Paths}
 
+
 object CsvParserRecursive {
 
   def run(pathFile: Path, select: Option[List[String]], types: Option[List[String => Any]]): Unit = {
+    // Crear una sesión de Spark
     val spark = SparkSession.builder.appName("CsvParserApp").master("local").getOrCreate()
 
-    val source = Source.fromFile(pathFile.toFile)
-    val lines = source.getLines()
+    try {
+      val source = Source.fromFile(pathFile.toFile)
+      val lines = source.getLines()
 
-    val result = parseCsvRec(lines, select, types)
+      // Parsear el CSV y construir el DataFrame
+      val data = parseCsvRec(lines, select, types, hasHeaders = true, silenceErrors = false)
+      println("Data que viene de parcer:")
+      println(data)
+      println("Obtenemos los encabezados")
+      val headers = select.getOrElse(List()) // Obtener los encabezados seleccionados
+      println(headers)
+      // Crear el esquema
+      val schema = StructType(
+        headers.map {
+          case "id_arbol"    => StructField("id_arbol", IntegerType, nullable = true)
+          case "altura_tot"  => StructField("altura_tot", IntegerType, nullable = true)
+          case "diametro"    => StructField("diametro", DoubleType, nullable = true)
+          case _             => throw new IllegalArgumentException("Encabezado no reconocido")
+        })
+      
+      val rows = data.map(row => headers.zip(row).map { case (header, value) => (header, value.toString) })
+      println("Printea la data de las rows:")
+      println(rows)
+      // Verificar que el número de columnas coincida
+      val numColumns = headers.length
+      val isValidData: Boolean = rows.forall(row => row.length == numColumns)
 
-    source.close()
+      if (isValidData) {
+       // Crear el DataFrame utilizando el esquema y las filas
 
-    // Mostrar solo las primeras 15 filas
-    result.take(15).foreach(println)
+           // Definir el esquema
+        
+
+        // Crear el rdd
+        val rdd = spark.sparkContext.parallelize(data.toSeq)
+        
+          // Printear el contenido del RDD
+        rdd.collect().foreach(println)
+
+        //val df = spark.createDataFrame(rdd, schema)
+
+        // Mostrar el DataFrame
+        //df.show()
+      } else {
+        println(s"Error: El número de columnas en los datos no coincide con el número de encabezados.")
+      }
+    } finally {
+      // Detener la sesión de Spark al finalizar
+      spark.stop()
+    }
   }
+
 
   /**
     * Parsea un archivo CSV de manera recursiva, con opciones para seleccionar columnas y especificar tipos de datos.
