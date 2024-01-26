2510fa9cf5a67ffb1443aca9cbbabd802ba5eedd
federico pfund <federicopfund@gmail.com>
2024-01-26 18:50:27 -0300

    /**   * feat: Agregar función plot para crear y guardar un gráfico de línea   *   * La función `plot` acepta secuencias de valores para los ejes x e y, así como un nombre de archivo   * para guardar el gráfico en la carpeta 'plotter' dentro del directorio 'resources'. Utiliza la biblioteca Breeze   * para generar un gráfico de línea y proporciona personalización de etiquetas y título.   *   * Detalles del cambio:   * - Se agrega una nueva función llamada `plot` en el código.   * - La función permite crear y guardar un gráfico de línea en formato PNG.   * - Se incluyen comentarios explicativos para cada sección del código.   * - Se muestra un ejemplo de uso al final del código.   *   * Ejemplo de uso:   *   * ```scala   * val xValues = Seq(1, 2, 3, 4, 5)   * val yValues = Seq(10, 20, 15, 25, 30)   * val fileName = "AlbumFigusPlot"   * plot(xValues, yValues, fileName)   * ```   */

--- Ejercicios/ejercicios_scala/unsam/src/main/scala/plotter/GeneralaPlot.scala
+++ Ejercicios/ejercicios_scala/unsam/src/main/scala/plotter/GeneralaPlot.scala
@@ -1,10 +1,10 @@
 package plotter
 
 import breeze.plot._
+import java.io.File
+import java.nio.file.{Files, Paths}
 
-/**
-  * Objeto `figus` que proporciona funciones para visualizar gráficos relacionados con la colección de figuritas.
-  */
+
 object figus {
   
   /**
@@ -12,6 +12,7 @@ object figus {
     *
     * @param x Secuencia de valores para el eje x.
     * @param y Secuencia de valores para el eje y.
+    * @param fileName Nombre del archivo de como se guardara.
     */
   def plot(x: Seq[Int], y: Seq[Int]): Unit = {
     // Crear una nueva figura y subparcela para el gráfico
@@ -28,5 +29,19 @@ object figus {
     
     // Refrescar la figura para mostrar el gráfico
     fig.refresh()
+    // Verificar si la carpeta 'plotter' existe en el directorio 'resources' y, si no, crearla
+   // Obtener la ruta del directorio 'resources' en el directorio de trabajo del proyecto
+    val resourcesPath = Paths.get("src", "resources")
+    
+    // Crear la ruta completa del directorio 'plotter' dentro de 'resources'
+    val plotterFolderPath = resourcesPath.resolve("plotter")
+
+    // Verificar si el directorio 'plotter' existe y, si no, crearlo
+    if (!Files.exists(plotterFolderPath)) {
+      Files.createDirectories(plotterFolderPath)
+    }
+    val fileName : String = "AlbumFigusPlot"
+    // Guardar el gráfico en la carpeta 'plotter' dentro de 'resources'
+    fig.saveas(s"src/resources/plotter/$fileName.png")
   }
 }
