e4c00dffe7538cdc374388e070311f29bb1ee442
federico pfund <federicopfund@gmail.com>
2024-01-26 16:32:58 -0300

    feat: Agregar función de búsqueda de precios para frutas en un archivo
    La función `precio` en el objeto `Buscador` permite buscar y obtener el precio de una fruta específica en un archivo de precios.
    
    Detalles del cambio:
    - Se ha creado el objeto `Buscador` con la función `precio` para facilitar la búsqueda de precios de frutas.
    - La función utiliza Scala.io.Source para leer el contenido del archivo de precios.
    - Itera sobre cada línea del archivo, compara la fruta buscada y actualiza el costo si encuentra una coincidencia.
    - Imprime un mensaje informativo si la fruta no está en el listado de precios.
    - Maneja posibles errores durante la búsqueda, como problemas al abrir el archivo.
    
    Ejemplo de uso:
    
    ```scala
    val filePath = Paths.get("ruta/del/archivo/precios.csv")
    val frutaBuscada = "Manzana"
    val precioEncontrado = Buscador.precio(frutaBuscada, filePath)
    println(s"El precio de $frutaBuscada es: $precioEncontrado")

--- Ejercicios/ejercicios_scala/unsam/src/main/scala/contable/BuscarPrecio.scala
+++ Ejercicios/ejercicios_scala/unsam/src/main/scala/contable/BuscarPrecio.scala
@@ -3,21 +3,36 @@ package contable
 import scala.io.Source
 import java.nio.file.Path
 
+/**
+  * Objeto Buscador para buscar el precio de una fruta en un archivo de precios.
+  */
 object Buscador {
+
+  /**
+    * Busca el precio de una fruta en un archivo y devuelve el costo.
+    *
+    * @param fruta    Nombre de la fruta cuyo precio se busca.
+    * @param filePath Ruta del archivo de precios.
+    * @return Precio de la fruta. Si la fruta no está en el listado, se imprime un mensaje y se devuelve 0.0.
+    */
   def precio(fruta: String, filePath: Path): Double = {
+    // Abre el archivo de precios utilizando Scala.io.Source
     val preciosFile = Source.fromFile(filePath.toString, "UTF-8")
 
     try {
       var costo = 0.0
+      // Itera sobre cada línea del archivo
       for (line <- preciosFile.getLines()) {
         val nuevaLinea = line.split(",")
         val opcion = nuevaLinea(0)
 
+        // Compara la fruta actual con la fruta buscada y actualiza el costo si coincide
         if (opcion == fruta) {
           costo = nuevaLinea(1).toDouble
         }
       }
 
+      // Imprime un mensaje si la fruta no está en el listado de precios
       if (costo == 0.0) {
         println(s"$fruta no figura en el listado de precios")
       }
@@ -25,9 +40,11 @@ object Buscador {
       costo
     } catch {
       case _: Throwable =>
+        // Imprime un mensaje de error si ocurre un problema durante la búsqueda
         println("Error al buscar el precio.")
         0.0
     } finally {
+      // Cierra el archivo al finalizar
       preciosFile.close()
     }
   }
